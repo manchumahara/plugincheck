e82ee2f662c2125b5c4fa991265387cf9d0f298a
Sabuj Kundu <manchumahara@gmail.com>
2014-09-22 14:49:34 +0600

    added support for j2.5 style

--- plugincheck.php
+++ plugincheck.php
@@ -25,21 +25,44 @@ class JFormFieldPlugincheck extends JFormField {
      */
     protected function getInput() {
 
-        
+
+        if(version_compare(JVERSION,'3.0.0','le')):
+            $doc = JFactory::getDocument();
+            $extracss = '
+                .label25{
+                    -webkit-border-radius: 3px;	-moz-border-radius: 3px;	border-radius: 3px;
+                    display: inline-block;
+                    padding: 2px 4px;
+                    font-size: 10.998px;
+                    font-weight: bold;
+                    line-height: 14px;
+                    color: #fff;
+                    vertical-align: baseline;
+                    white-space: nowrap;
+                    text-shadow: 0 -1px 0 rgba(0,0,0,0.25);
+                    background-color: #999;
+                }
+                .label-success{ background-color: #468847; }
+                .label-warning{ background-color: #468847; }
+                .label-info{ background-color: #468847; }
+            ';
+            $doc->addStyleDeclaration($extracss);
+         endif;
+
         $folder     = $this->element['folder'];
         $element    = $this->element['element'];
 
         if(!empty($folder) && !empty($element) ){
             $enabled = JPluginHelper :: isEnabled  ($folder, $element);
             if($enabled){
-                return '<span class="label label-success">'.$folder. ' plugin "'.$element.'" is enabled</span>';
+                return '<span class="label label25 label-success">'.$folder. ' plugin "'.$element.'" is enabled</span>';
             }
             else{
-                return '<span class="label label-warning">'.$folder. ' plugin "'.$element.'" is not enabled</span>';
+                return '<span class="label label25 label-warning">'.$folder. ' plugin "'.$element.'" is not enabled</span>';
             }
         }
         else{
-            return '<span class="label label-info">Field is not configured correctly!</span>';
+            return '<span class="label label25 label-info">Field is not configured correctly!</span>';
         }
 
     }
